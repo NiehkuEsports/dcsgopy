98a3de5fcf17814f61c153562389306629bd5c25
Eskil Opdahl Nordland <eskil.nordland@gmail.com>
2017-02-03 23:52:18 +0100

    db stuff

--- csgo/db/databse.py
+++ csgo/db/databse.py
@@ -17,6 +17,9 @@ class Database(object):
 						(integer start, integer end, integer start_tv, integer end_tv)''')
 		c.execute('''INSERT INTO port_range VALUES ({}, {}, {}, {})'''.format(start, end, start_tv, end_tv))
 
+		c.execute('''CREATE TABLE csgo_container
+						(integer port, integer tv_port, text name''')
+
 		self.close()
 
 	def destroy(self):
@@ -32,12 +35,64 @@ class Database(object):
 		self.conn.close()
 		delattr(self, '_conn')
 
+	def insert_csgo_container(self, port, tv_port, name):
+		c = self.conn.cursor()
+		ports = self.get_port_range()
+		if ports['start'] <= port <= ports['end']:
+			self.close()
+			raise IndexError('port {} out of range: {}-{}'.format(port, ports['start'], ports['end']))
+
+		if ports['start_tv'] <= port <= ports['end_tv']:
+			self.close()
+			raise IndexError('tv port {} out of range: {}-{}'.format(tv_port, ports['start_tv'], ports['end_tv']))
+
+		if c.execute("SELECT port from csgo_container WHERE port={}".format(port)):
+			self.close()
+			raise IndexError('port {} is already in use'.format(port))
+
+		if c.execute("SELECT tv_port from csgo_container WHERE tv_port={}".format(tv_port)):
+			self.close()
+			raise IndexError('tv port {} is already in use'.format(port))
+
+		if c.execute("SELECT name from csgo_container WHERE name={}".format(name)):
+			self.close()
+			raise IndexError('name {} is already in use'.format(port))
+
+		c.execute('''INSERT INTO csgo_container VALUES ({}, {}, {})'''.format(port, tv_port, name))
+		self.close()
+
 	def get_port_range(self):
 		c = self.conn.cursor()
 		ports = c.execute("SELECT * FROM port_range")[0]
+		self.close()
 		return {
 			'start': ports[0],
 			'end': ports[1],
 			'start_tv': ports[2]
 			'end_tv': ports[3]
 		}
+
+	def get_max_servers(self):
+		c = self.conn.cursor()
+		ports = c.execute("SELECT * FROM port_range")[0]
+		self.close()
+
+		return ports[1] - ports[0]
+
+	def get_available_ports(self):
+		port_range = self.get_port_range()
+		c = self.conn.cursor()
+		ports = c.execute('''SELECT port FROM csgo_container''')
+		tv_ports = c.execute('''SELECT tv_port FROM csgo_container''')
+		available_tv_ports = []
+		available_ports = []
+		for port in range(port_range['start'], port_range['end']):
+			if port not in ports:
+				available_ports.append(port)
+
+		for tv_port in range(port_range['tv_start'], port_range['tv_end']):
+			if tv_port not in tv_ports:
+				available_tv_ports.append(tv_port)
+
+		return available_ports, available_tv_ports
+		self.close()
